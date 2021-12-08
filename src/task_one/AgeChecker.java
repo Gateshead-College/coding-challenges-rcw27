d5bd819f0663d67e05de460ba59706df22bf7d1b
Cowan-Wilce <rosie.cowan-wilce@accenture.com>
2021-12-08 09:58:48 +0000

    Task 1 Completed and tested

--- src/task_one/AgeChecker.java
+++ src/task_one/AgeChecker.java
@@ -18,12 +18,26 @@ public class AgeChecker {
         System.out.println("How old are you?");
         int age = Integer.parseInt(scn.nextLine());
         String word = "age";
-        if(age <=16){
-            System.out.println("You are still so young!");
+        if(age <16){
+            System.out.println("You are so wittle and cute XD!");
         }
-        else{
-            System.out.println("You are old!");
+        if(age>=16 && age <20){
+            System.out.println("You are still young, don't wish it away Padawan.");
         }
+        if(age>=20 && age <25){
+            System.out.println("You can now eat whatever you want for breakfast, you're in charge!");
+        }
+        if(age>=25 && age <30){
+            System.out.println("It's really not THAT scary being 30 but maybe start using an eye cream.");
+        }
+        if(age>=30 && age <40){
+            System.out.println("You are who you are now, well done for discovering that. You're awesome!");
+        }
+        if(age>=40 && age <50){
+            System.out.println("About time for a mid-life crisis? Yeah, go buy something crazy...like a lion or a sports car. Or...A LION IN A SPORTS CAR!!!");
+        }if(age>=50){
+            System.out.println("AND...relax. Just do what you want now mate. You earned it.");        }
+
 
 
     }
