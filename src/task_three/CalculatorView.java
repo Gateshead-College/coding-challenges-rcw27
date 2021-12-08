0c4f9820c84aac644be41a2c2072571db1de0f52
Cowan-Wilce <rosie.cowan-wilce@accenture.com>
2021-12-08 13:57:57 +0000

    Coded to give exit option-still debugging

--- src/task_three/CalculatorView.java
+++ src/task_three/CalculatorView.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class CalculatorView {
 
+
     public static void main(String[] args) {
         CalculatorView cv = new CalculatorView();
         cv.printMenu();
     }
+
 
     private void printMenu() {
 
@@ -19,14 +21,14 @@ public class CalculatorView {
         //double allows for a decimal point answer - needed for potential divide functions
 
         Scanner scn = new Scanner(System.in);
+        String answer;
 
-        System.out.println("Firstly, which operation would you like to perform?");
+        System.out.println("Which operation would you like to perform?");
         System.out.println("You are currently able to use: +, -, * or /");
         operator = scn.next().charAt(0);
 
         System.out.println("What is the first number you'd like to input?");
         num1 = scn.nextDouble();
-
 
         System.out.println("And what is the second number you would you to input?");
         num2 = scn.nextDouble();
@@ -35,31 +37,34 @@ public class CalculatorView {
             case '+':
                 result = num1 + num2;
                 System.out.println("Your answer to " + num1 + " + " + num2 + " = " + result);
-                //System.out.println("Please try another sum or press '0' to exit");
-                printMenu();
-                case '-':
+                break;
+            case '-':
                 result = num1 - num2;
                 System.out.println("Your answer to " + num1 + " - " + num2 + " = " + result);
-                //System.out.println("Please try another sum or press '0' to exit");
-                printMenu();
-                case '*':
+                break;
+
+            case '*':
                 result = num1 * num2;
                 System.out.println("Your answer to " + num1 + " * " + num2 + " = " + result);
-                //System.out.println("Please try another sum or press '0' to exit");
-                printMenu();
-                case '/':
+                break;
+
+            case '/':
                 result = num1 / num2;
                 System.out.println("Your answer to " + num1 + " / " + num2 + " = " + result);
-               // System.out.println("Please try another sum or press '0' to exit");
-                printMenu();
-
-            default:
-                System.exit(0);
+                break;
 
 
         }
+        do {
+            System.out.println("Would you like to do another calculation? Y/N");
+            answer = scn.nextLine();
 
+            if (answer.equalsIgnoreCase("y")) {
+                printMenu();
+            } else if (answer.equalsIgnoreCase("n")) {
+                System.exit(0);
+            }
+        }
     }
-    //add in function to work out answer based on the operator the user has input
 }
 
