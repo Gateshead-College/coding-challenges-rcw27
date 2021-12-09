2e6e6fb51e7bf509e8387aa186572e42560c2a63
Cowan-Wilce <rosie.cowan-wilce@accenture.com>
2021-12-09 11:43:51 +0000

    Functionality restored after edits, changes made for increased readability in console

--- src/task_three/CalculatorView.java
+++ src/task_three/CalculatorView.java
@@ -6,17 +6,27 @@ import java.util.Scanner;
 
 public class CalculatorView {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
 
     public static void main(String[] args) {
         CalculatorView cv = new CalculatorView();
-        cv.printMenu();
+        cv.getOperator();
     }
 
 
-    private void printMenu() {
+    private void getOperator() {
 
         char operator;
-        double num1, num2, result;
+        double num1, num2, result, num3, newResult;
 
         //double allows for a decimal point answer - needed for potential divide functions
 
@@ -36,36 +46,66 @@ public class CalculatorView {
         switch (operator) {
             case '+' -> {
                 result = num1 + num2;
-                System.out.println("Your answer to " + num1 + " + " + num2 + " = " + result);
+                System.out.println();
+                System.out.println(ANSI_PURPLE + "Your answer to " + num1 + " + " + num2 + " = " + result + ANSI_RESET);
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
             }
+
             case '-' -> {
                 result = num1 - num2;
-                System.out.println("Your answer to " + num1 + " - " + num2 + " = " + result);
+                System.out.println();
+                System.out.println(ANSI_PURPLE + "Your answer to " + num1 + " - " + num2 + " = " + result + ANSI_RESET);
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
             }
+
             case '*' -> {
                 result = num1 * num2;
-                System.out.println("Your answer to " + num1 + " * " + num2 + " = " + result);
+                System.out.println();
+                System.out.println(ANSI_PURPLE + "Your answer to " + num1 + " * " + num2 + " = " + result + ANSI_RESET);
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
             }
             case '/' -> {
                 result = num1 / num2;
-                System.out.println("Your answer to " + num1 + " / " + num2 + " = " + result);
+                System.out.println();
+                System.out.println(ANSI_PURPLE + "Your answer to " + num1 + " / " + num2 + " = " + result + ANSI_RESET);
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
             }
-
         }
-        System.out.println("What would you like to do now?");
-        System.out.println("1 - Another Calculation");
-        System.out.println("2 - Exit");
 
-        int answer = Integer.parseInt(newScanner.nextLine());
 
-        if (answer == 1) {
-            printMenu();
+                System.out.println("What would you like to do now?");
+                System.out.println("1 - Perform a different Calculation");
+                System.out.println("2 - Exit");
+
+                int answer = Integer.parseInt(newScanner.nextLine());
+
+                if (answer == 1) {
+                    getOperator();
+
+                } else if (answer == 2) {
+                    System.out.println("Bye");
+                }
+
+            }
         }
-        else if (answer == 2) {
-            System.out.println("Bye");
-        }
-    }
-}
+
+
+
+
 
 
 
